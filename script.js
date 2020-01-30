064c835f1de4006e951008c2c3e78d29a300eae1
James Calverley <james.calverley3@gmail.com>
2020-01-29 21:57:43 -0500

    added input fields

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ generateBtn.addEventListener("click", writePassword);
 
 //Work 
 
+
+function getCharNum (){
+  var charWanted = prompt("Enter the number of characters you want your password to be \n 1 - 128 ");
+  console.log(`characters requested: ${charWanted}`);
+}
+
+var upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+
+console.log(upperArray);
+console.log(lowerArray);
+console.log(numberArray);
+console.log(specialArray);
+
+
+
+
+
+
+
