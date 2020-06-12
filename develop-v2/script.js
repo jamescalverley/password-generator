a8aa0903a5ea40b5f0f2deaf4b61f0a8aaaeb8d4
James Calverley <james.calverley3@gmail.com>
2020-06-12 17:20:43 -0400

    creates password array and converts to string

--- develop-v2/script.js
+++ develop-v2/script.js
@@ -3,12 +3,18 @@
 // add function to add desired number of characters to password string
 // add alerts if charNeeded is below 8 or over 128
 
+
+
+// adding characters x number of times
+// each time adding a random character (index must be between 0 and string.length)
+
 const lowerString = "abcdefghijklmnopqrstuvwxyz";
 const upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numberString = "0123456789";
 const specialString = "!@#$%^&*()_+";
 
 let newPassword = "";
+let passArray = [];
 
 function charactersNeeded(){
     let charNeeded = document.getElementById('charNeeded').value;
@@ -18,13 +24,24 @@ function charactersNeeded(){
         console.log("ADD ALERT: charNeeded above 128")
     } else {
         console.log(`Characters needed ${charNeeded}`);
+    return charNeeded
     };
 };
 
-function addCharacters( charNumber ){
-    console.log(`adding character... ${charNumber} needed`)
-    let add = lowerString[ charNumber ];
-    console.log(`Adding character: ${add}`)
+function addCharacters( charNeeded ){
+    console.log(`adding character... ${charNeeded} needed`)
+    
+    let count = 0;
+    while (count < charNeeded){
+        passArray.push(lowerString[1])
+        count ++
+    };
+    console.log(`test array: ${passArray}`)
+    newPassword = passArray.join('');
+    console.log(`converted to string: ${newPassword}`)
+    
+    return newPassword
+    
 }
 
 let numArray = [];
@@ -39,9 +56,11 @@ function generatePass(){
     console.log("Generate btn clicked...")
     charactersNeeded();
     randomNum();
-    addCharacters(3)
+    addCharacters( charactersNeeded() );
 };
 
 document.getElementById('generate').addEventListener('click', generatePass );
+
+
 
 
