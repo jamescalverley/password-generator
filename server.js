22200c419ea3a8b5ec874143e4df8cbddb68197e
James Calverley <james.calverley3@gmail.com>
2021-03-29 19:23:48 -0400

    restructures project

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/', express.static('public'));
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
 
 app.listen( PORT, () => {
     console.log(`Password Generator running on http://localhost:${PORT}`)
